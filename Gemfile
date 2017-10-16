dcf9161f371c4f4a68f89cc4aaaeabcc1e83feba
Vinodh Venkatesan <vinvvk@hotmail.com>
2017-10-16 00:50:05 +0000

    New changes to the repo

--- Gemfile
+++ Gemfile
@@ -1,5 +1,16 @@
 # A sample Gemfile
 source 'https://rubygems.org'
-
-gem "puppet", "~> 4.5.0"
-gem "puppetlabs_spec_helper", "~> 1.1.1"
+ 
+group :tests do
+  gem 'puppetlabs_spec_helper', :require => false
+end
+ 
+group :system_tests do
+  gem 'beaker',       :require => false
+  gem 'beaker-rspec', :require => false
+  gem 'beaker-puppet_install_helper', :require => false
+end
+ 
+gem 'facter'
+gem 'puppet'
+gem 'fog-aws'
---
+++ spec/acceptance/test_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper_acceptance'
+ 
+describe 'certmonger' do
+  let(:manifest) {
+    <<-EOS
+include certmonger
+EOS
+  }
+  it 'should apply without errors' do
+    apply_manifest(manifest, :catch_failures => true)
+  end
+ 
+  it 'should apply a second time without changes' do
+    @result = apply_manifest(manifest)
+    expect(@result.exit_code).to be_zero
+  end
+  describe service('certmonger') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
