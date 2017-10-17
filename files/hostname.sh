591b25e8d80b86b650a799106a33f6ecc932c031
Vinodh Venkatesan <vinodh.venkatesan@saltedsignal.com.au>
2017-10-17 22:51:07 +0000

    Modified vm tag name to centos-7, added new hostname.sh script, using run_script_on in helper ruby script

--- files/hostname.sh
+++ files/hostname.sh
@@ -2,6 +2,6 @@ hostname centos.local
 export HOSTNAME="centos.local"
 IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 cat > /etc/hosts < EOF
-127.0.0.1 localhost
-::1 localhost localhost6
-$IP centos.local
+127.0.0.1 localhost.localdomain localhost
+$IP centos-7.local centos-7
+EOF
