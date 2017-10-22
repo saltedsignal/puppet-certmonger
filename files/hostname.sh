a7c69134da69daf593d98c30ace70de197f78c04
Vinodh Venkatesan <vinodh.venkatesan@saltedsignal.com.au>
2017-10-22 23:40:12 +0000

    changes to hostname.sh

--- files/hostname.sh
+++ files/hostname.sh
@@ -1,7 +1,8 @@
 hostname centos.local
 export HOSTNAME="centos.local"
+echo "centos.local" | cat > /etc/hostname
 IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-cat > /etc/hosts < EOF
+cat > /etc/hosts << EOF
 127.0.0.1 localhost.localdomain localhost
 $IP centos-7.local centos-7
 EOF
