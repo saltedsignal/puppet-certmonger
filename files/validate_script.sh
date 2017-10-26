61968b3cfce8fcdb97f78540fa41b3b868ae9e63
Vinodh Venkatesan <vinodh.venkatesan@saltedsignal.com.au>
2017-10-26 08:00:56 +0000

    validate script to check if status is monitoring

--- files/validate_script.sh
+++ files/validate_script.sh
@@ -1,3 +1,7 @@
 status=$(ipa-getcert list -f '/tmp/server.crt' | grep 'status' | awk 'BEGIN{fs=":"};{print $2};' | tr -d ' ')
-if [ "$status" != "MONITORING" ]
+if [ "$status" == "MONITORING" ];
+then
+  exit 0
+else
   exit 1
+fi
