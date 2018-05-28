91e4f9c6e180e15c30f318a055132d7d0581ca1d
Arx Cruz <arxcruz@redhat.com>
2018-05-28 16:20:17 +0200

    Restarting DBus before install certmonger
    If DBus is updated before run this module and the machine not get
    restarted, the certmonger fails (among other services that relies
    on dbus service).

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,15 @@
 # Class: certmonger
 class certmonger {
+  # DBus package update from 7.4 to 7.5 breaks
+  # The certmonger service, so dbus must get a
+  # restart prior to start certmonger.
+  # The official solution for dbus is either
+  # restart the service, or reboot the machine
+  service { 'dbus':
+    ensure => running,
+    enable => true,
+    restart => '',
+  }
 
   package { 'certmonger':
     ensure => 'present',
