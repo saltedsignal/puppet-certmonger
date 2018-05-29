223325f86731f110104764bef6bbfd249cea20d0
Arx Cruz <arxcruz@redhat.com>
2018-05-29 10:35:29 +0200

    Adding manage_dbus_service option
    Adding an option to restart dbus service

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,18 @@
 # Class: certmonger
-class certmonger {
+class certmonger(
+  $manage_dbus_service = false
+) {
   # DBus package update from 7.4 to 7.5 breaks
   # The certmonger service, so dbus must get a
   # restart prior to start certmonger.
   # The official solution for dbus is either
   # restart the service, or reboot the machine
-  service { 'dbus':
-    ensure => running,
-    enable => true,
-    restart => '',
+  if $manage_dbus_service {
+    service { 'dbus':
+      ensure => running,
+      enable => true,
+      restart => '',
+    }
   }
 
   package { 'certmonger':
